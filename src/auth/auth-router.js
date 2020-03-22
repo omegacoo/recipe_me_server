bc124916937be702f2c9ff85d7cebb411f74bac9
omegacoo <bsumser@yahoo.com>
2020-03-22 13:28:35 -0700

    added sameSite and secure as recommended by google

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -45,7 +45,7 @@ authRouter
                     algorithm: 'HS256',
                     expiresIn: jwtExpirySeconds
                 });
-                res.cookie('token', token, { maxAge: jwtExpirySeconds * 1000, httpOnly: true });
+                res.cookie('token', token, { maxAge: jwtExpirySeconds * 1000, httpOnly: true, sameSite: none, secure });
                 res.end();
             })
             .catch(next)
