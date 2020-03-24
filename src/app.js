739f4157cd23810e5b1cfbd0c77a392ecd73740c
omegacoo <bsumser@yahoo.com>
2020-03-24 11:00:42 -0700

    put content-type in allowed headers... derp

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 const corsOptions = {
     origin: ['http://localhost:3000', 'https://pantry-buddy.com'],
     credentials: true,
-    allowedHeaders: ['Cookie', 'Set-Cookie']
+    allowedHeaders: ['Cookie', 'Set-Cookie', 'Content-Type']
 };
 
 app.use(morgan(morganOption));
